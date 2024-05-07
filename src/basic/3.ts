054dae4ceec3d7a0a09f3b150f91b4cd49ff0633
AndreyZaycev1999 <zaycev.andrey.1999@gmail.com>
2024-05-07 16:36:44 +0300

    1

---
+++ src/basic/3.ts
@@ -0,0 +1,9 @@
+let someVar: string|number;
+someVar = 'hello';
+console.log(someVar);
+someVar = 12345;
+console.log(someVar);
+
+let someBool: 'enable' | 'disable';
+someBool = 'enable';
+console.log(someBool);
